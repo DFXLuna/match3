ba8911b9489bba69760e1bbf1d2edcbea0ddbcb0
Matt Grant <teamuba@gmail.com>
2017-12-16 16:12:56 -0700

    Reorganized

--- Assets/_script/_nonunity/Board.cs
+++ Assets/_script/_nonunity/Board.cs
@@ -2,30 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TileManager : MonoBehaviour {
-	public Transform tile;
+public class Board {
+	private Transform tile;
 	private List<Transform> tiles;
-	// For testing
-	public int testRows = 0;
-	public int testCols = 0;
-	public Transform testJewel;
-	private List<Transform> testJewels;
 
-	void Start () {
+	public Board( int rows, int cols, Transform tile ) {
+		this.tile = tile;
 		tiles = new List<Transform>();
-		createGrid( testRows, testCols );
-		testJewels = new List<Transform>();
-		// jewel test
-		Vector3 pos = tiles[0].position;
-		pos.y += .51f;
-		testJewels.Add( Object.Instantiate( testJewel, pos, testJewel.rotation ) );
+		createGrid( rows, cols );
 	}
 
-	void Update () {
-		
-	}
-
-	void createGrid( int r, int c ){
+	private void createGrid( int r, int c ){
 		Vector3 pos;
 		pos.y = 0;
 		// Figure out where to position the grid tiles.
@@ -33,6 +20,7 @@ public class TileManager : MonoBehaviour {
 		float distY = c / 2;
 		if( r % 2 == 0 ){ distX -= .5f; }
 		if( c % 2 == 0 ){ distY -= .5f; }
+		// Populate the board
 		for( int i = 0; i < r; i++ ){
 			for( int j = 0; j < c; j++ ){
 				pos.x = i + - distX;
