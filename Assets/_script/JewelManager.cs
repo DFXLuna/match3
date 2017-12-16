38ccdd319589e26919b8dbb002d625bd4eea7e30
Matt Grant <teamuba@gmail.com>
2017-12-16 11:38:14 -0700

    init

---
+++ Assets/_script/JewelManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JewelManager : MonoBehaviour {
+	public Transform testJewel;
+	private List<Transform> testJewels;
+	void Start () {
+		testJewels = new List<Transform>();
+		testJewels.Add( Object.Instantiate( testJewel, pos, testJewel.rotation ) );
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/_script/TileManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileManager : MonoBehaviour {
+	public Transform tile;
+	private List<Transform> tiles;
+	// For testing
+	public int testRows = 0;
+	public int testCols = 0;
+	public Transform testJewel;
+	private List<Transform> testJewels;
+
+	void Start () {
+		tiles = new List<Transform>();
+		createGrid( testRows, testCols );
+		testJewels = new List<Transform>();
+		// jewel test
+		Vector3 pos = tiles[0].position;
+		pos.y += .51f;
+		testJewels.Add( Object.Instantiate( testJewel, pos, testJewel.rotation ) );
+	}
+
+	void Update () {
+		
+	}
+
+	void createGrid( int r, int c ){
+		Vector3 pos;
+		pos.y = 0;
+		// Figure out where to position the grid tiles.
+		float distX = r / 2;
+		float distY = c / 2;
+		if( r % 2 == 0 ){ distX -= .5f; }
+		if( c % 2 == 0 ){ distY -= .5f; }
+		for( int i = 0; i < r; i++ ){
+			for( int j = 0; j < c; j++ ){
+				pos.x = i + - distX;
+				pos.z = j + - distY;
+				tiles.Add( Object.Instantiate( tile, pos, Quaternion.identity ) );
+			}
+		}
+	}
+}
